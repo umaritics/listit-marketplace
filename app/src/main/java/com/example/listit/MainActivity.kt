b2d59283485b8a8a2f6de6c15b809888fb3b107e
umaritics <umar.irpk@gmail.com>
2025-10-29 01:05:35 +0500

    RELNOTES: Created splash screen and added Images for OB screens

--- app/src/main/java/com/example/listit/MainActivity.kt
+++ app/src/main/java/com/example/listit/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.listit
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Delay in milliseconds (e.g., 2000 = 2 seconds)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, OB_screen1::class.java)
+            startActivity(intent)
+            finish() // finish this activity so user can't return to it
+        }, 2000)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
