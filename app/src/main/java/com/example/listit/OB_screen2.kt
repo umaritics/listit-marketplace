0c5b7db8ec085ee76e22a86f38caec90168f4f84
umaritics <umar.irpk@gmail.com>
2025-10-29 01:06:19 +0500

    RELNOTES: Created On Boarding Screen 2

---
+++ app/src/main/java/com/example/listit/OB_screen2.kt
@@ -0,0 +1,28 @@
+package com.example.listit
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+
+class OB_screen2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ob_screen2)
+        var next = findViewById<MaterialButton>(R.id.next_button)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        next.setOnClickListener {
+            startActivity(Intent(this, OB_screen3::class.java))
+            finish()
+        }
+    }
+}
