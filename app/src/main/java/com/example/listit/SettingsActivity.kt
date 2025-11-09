241b1032a8758d6513c8a82235a5782b646d5714
usman <i230585@isb.nu.edu.pk>
2025-11-09 21:57:19 +0500

    Notification-Settings-3-Pages-Draft-1

---
+++ app/src/main/java/com/example/listit/SettingsActivity.kt
@@ -0,0 +1,31 @@
+package com.example.listit
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+
+
+        setContentView(R.layout.activity_settings)
+
+        findViewById<ImageView>(R.id.arrowNotification).setOnClickListener {
+            val intent = Intent(this, NotificationsSettings_Activity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
